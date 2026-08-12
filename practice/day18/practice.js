82c5f74d6396d2edccb37fb5a772ce8376278126
tariku negash <tarikunegash447@gmail.com>
2026-08-12 21:35:49 +0300

    day 18 the  practice.js is  pushed

---
+++ practice/day18/practice.js
@@ -0,0 +1,22 @@
+const products = [
+  { name: "Laptop", price: 1000 },
+  { name: "Phone", price: 500 },
+  { name: "Headphones", price: 100 },
+];
+
+const names = products.map(product => product.name);
+
+console.log(names);
+
+const customer = {
+    name: "tariku",
+    age: 24,
+    city: "addis Ababa"
+
+}
+console.log(customer.name)
+customer.age = 25
+console.log(customer)
+customer.subCity = "kality"
+console.log(customer)
+
